76030d2868e8de5af63633122dd19f263aad15da
Julian Haupt <julian.haupt@hauptmedia.de>
2015-09-23 23:07:51 +0200

    add ftp user if it was specified by environment variable

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # docker-proftpd
+
+Example run for a proftpd instance:
+
+```bash
+docker run -d -p 20:20 -p 21:21 \
+-e FTP_USERNAME=test -e FTP_PASSWORD=test \
+hauptmedia/proftpd
+```
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+if [ -n "$FTP_USERNAME" -a -n "$FTP_PASSWORD" ]; then
+	CRYPTED_PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $FTP_PASSWORD)
+	useradd --shell /bin/sh --create-home --password $CRYPTED_PASSWORD $FTP_USERNAME
+fi
+
 exec "$@"
