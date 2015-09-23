35898a1c4be40e58b57a4b1a3141cc3f288b6416
Julian Haupt <julian.haupt@hauptmedia.de>
2015-09-23 23:16:52 +0200

    create empty home directory (without bash profile)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,9 @@
 
 if [ -n "$FTP_USERNAME" -a -n "$FTP_PASSWORD" ]; then
 	CRYPTED_PASSWORD=$(perl -e 'print crypt($ARGV[0], "password")' $FTP_PASSWORD)
-	useradd --shell /bin/sh --create-home --password $CRYPTED_PASSWORD $FTP_USERNAME
+	mkdir /home/$FTP_USERNAME
+	useradd --shell /bin/sh -d /home/$FTP_USERNAME --password $CRYPTED_PASSWORD $FTP_USERNAME
+	chown -R $FTP_USERNAME:$FTP_USERNAME /home/$FTP_USERNAME
 fi
 
 exec "$@"
